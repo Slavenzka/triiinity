c8df334fa54a32ebf053071de0c5acdbbca7ed1d
Ivan <i.romanov@protonmail.com>
2020-11-23 12:53:06 +0300

    Added slider

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+import { Swiper, Autoplay, EffectFade } from 'swiper'
+
+Swiper.use([Autoplay, EffectFade])
+
+new Swiper('.swiper-container', {
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 1000,
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  simulateTouch: false,
+  allowTouchMove: false,
+})
+
 console.log('Hello webpack');
