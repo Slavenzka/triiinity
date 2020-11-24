cd316a94c81a1b1d455131309f9871d4e1374912
Ivan <i.romanov@protonmail.com>
2020-11-24 11:35:13 +0300

    Fixed scroll

--- public/bundle.js
+++ public/bundle.js
@@ -11839,7 +11839,7 @@ class PageController {
 
   _handleClickButton () {
     this.targetObject.scrollTo({
-      top: screen.height,
+      top: window.innerHeight,
       behavior: 'smooth'
     });
   }
